d5efccb251247b7ee0c990f63a29c3c93af63bcb
twinkle90-pal <twinklepal13@gmail.com>
2025-06-30 01:09:47 +0530

    galleryimg

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -8,13 +8,13 @@ export default function Gallery() {
 <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
     <div className="grid gap-4">
         <div>
-            <img className="h-auto max-w-full  rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image.jpg" alt=""/>
+            <img className="h-auto max-w-full  rounded-lg" src="https://cdn.pixabay.com/photo/2022/08/26/07/57/tavern-7411977_1280.jpg" alt=""/>
         </div>
         <div>
-            <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-1.jpg" alt=""/>
+            <img className="h-auto max-w-full rounded-lg" src="https://cdn.pixabay.com/photo/2021/10/14/15/11/cathedral-6709412_1280.jpg" alt=""/>
         </div>
         <div>
-            <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-2.jpg" alt=""/>
+            <img className="h-auto max-w-full rounded-lg" src="https://cdn.pixabay.com/photo/2019/03/09/21/30/downtown-4045036_1280.jpg" alt=""/>
         </div>
     </div>
     <div className="grid gap-4">
@@ -22,7 +22,7 @@ export default function Gallery() {
             <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-3.jpg" alt=""/>
         </div>
         <div>
-            <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-4.jpg" alt=""/>
+            <img className="h-auto max-w-full rounded-lg" src="https://cdn.pixabay.com/photo/2022/08/26/07/57/tavern-7411977_1280.jpg" alt=""/>
         </div>
         <div>
             <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-5.jpg" alt=""/>
