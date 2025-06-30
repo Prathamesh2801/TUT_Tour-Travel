70e481c64ea0f35cc0d8b9c37119f34b9e8da2d5
twinkle90-pal <twinklepal13@gmail.com>
2025-06-30 15:22:24 +0530

    galleryimg

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -5,51 +5,92 @@ export default function Gallery() {
     
         
 
-<div className="grid grid-cols-2 md:grid-cols-4 gap-4">
-    <div className="grid gap-4">
-        <div>
-            <img className="h-auto max-w-full  rounded-lg" src="https://cdn.pixabay.com/photo/2022/08/26/07/57/tavern-7411977_1280.jpg" alt=""/>
-        </div>
-        <div>
-            <img className="h-auto max-w-full rounded-lg" src="https://cdn.pixabay.com/photo/2021/10/14/15/11/cathedral-6709412_1280.jpg" alt=""/>
-        </div>
-        <div>
-            <img className="h-auto max-w-full rounded-lg" src="https://cdn.pixabay.com/photo/2019/03/09/21/30/downtown-4045036_1280.jpg" alt=""/>
-        </div>
+<div className="grid grid-cols-2 gap-4 md:grid-cols-4 p-4 m-5">
+  <div className="grid gap-4">
+    <div>
+      <img
+        className="h-auto max-w-full rounded-lg object-cover object-center"
+        src="https://cdn.pixabay.com/photo/2018/05/16/17/49/madrid-3406578_1280.jpg"
+        alt="gallery-photo"
+      />
     </div>
-    <div className="grid gap-4">
-        <div>
-            <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-3.jpg" alt=""/>
-        </div>
-        <div>
-            <img className="h-auto max-w-full rounded-lg" src="https://cdn.pixabay.com/photo/2022/08/26/07/57/tavern-7411977_1280.jpg" alt=""/>
-        </div>
-        <div>
-            <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-5.jpg" alt=""/>
-        </div>
+    <div>
+      <img
+        className="h-auto max-w-full rounded-lg object-cover object-center "
+        src="https://cdn.pixabay.com/photo/2019/06/09/21/54/santorini-4263036_1280.jpg"
+        alt="gallery-photo"
+      />
     </div>
-    <div className="grid gap-4">
-        <div>
-            <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-6.jpg" alt=""/>
-        </div>
-        <div>
-            <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-7.jpg" alt=""/>
-        </div>
-        <div>
-            <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-8.jpg" alt=""/>
-        </div>
+    <div>
+      <img
+        className="h-auto max-w-full rounded-lg object-cover object-center"
+        src="https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=2940&amp;q=80"
+        alt="gallery-photo"
+      />
     </div>
-    <div className="grid gap-4">
-        <div>
-            <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-9.jpg" alt=""/>
-        </div>
-        <div>
-            <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-10.jpg" alt=""/>
-        </div>
-        <div>
-            <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-11.jpg" alt=""/>
-        </div>
+  </div>
+  <div className="grid gap-4">
+    <div>
+      <img
+        className="h-auto max-w-full rounded-lg object-cover object-center"
+        src="https://cdn.pixabay.com/photo/2020/02/11/16/25/pisa-4840084_1280.jpg"
+        alt="gallery-photo"
+      />
     </div>
+    <div>
+      <img
+        className="h-auto max-w-full rounded-lg object-cover object-center"
+        src="https://cdn.pixabay.com/photo/2019/10/18/17/42/season-4559795_1280.jpg"
+        alt="gallery-photo"
+      />
+    </div>
+    <div>
+      <img
+        className="h-auto max-w-full rounded-lg object-cover object-center "
+        src="https://cdn.pixabay.com/photo/2022/04/27/12/30/switzerland-7160287_1280.jpg"
+        alt="gallery-photo"
+      />
+    </div>
+  </div>
+  <div className="grid gap-4">
+    <div>
+      <img
+        className="h-auto max-w-full rounded-lg object-cover object-center"
+        src="https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=2940&amp;q=80"
+        alt="gallery-photo"
+      />
+    </div>
+    <div>
+      <img
+        className="h-auto max-w-full rounded-lg object-cover object-center "
+        src="https://cdn.pixabay.com/photo/2019/09/29/06/10/church-4512404_1280.jpg"
+        alt="gallery-photo"
+      />
+    </div>
+    <div>
+      <img
+        className="h-auto max-w-full rounded-lg object-cover object-center"
+        src="https://cdn.pixabay.com/photo/2014/09/28/16/42/barcelona-465002_1280.jpg"
+        alt="gallery-photo"
+      />
+    </div>
+  </div>
+  <div className="grid gap-4">
+    <div>
+      <img
+        className="h-auto max-w-full rounded-lg object-cover object-center"
+        src="https://images.unsplash.com/photo-1552960562-daf630e9278b?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=687&amp;q=80"
+        alt="gallery-photo"
+      />
+    </div>
+    <div>
+      <img
+        className="h-auto max-w-full rounded-lg object-cover object-center"
+        src="https://cdn.pixabay.com/photo/2023/09/25/20/38/lisbon-8275994_1280.jpg"
+        alt="gallery-photo"
+      />
+    </div>
+  </div>
 </div>
 
   
